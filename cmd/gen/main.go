5c5dee827f16c3255540fd75ab84038bcee5e6a9
Tony Bai <bigwhite.cn@aliyun.com>
2020-12-04 16:29:25 +0800

    cmd/gen: move rewrite to pkg/generator/rewrite.go

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
-	"go/ast"
-	"go/format"
-	"go/parser"
-	"go/token"
 	"os"
 	"strings"
 
-	"golang.org/x/tools/go/ast/astutil"
+	"github.com/bigwhite/functrace/pkg/generator"
 )
 
 var (
@@ -51,7 +46,7 @@ func main() {
 		return
 	}
 
-	newSrc, err := rewrite(file, nil)
+	newSrc, err := generator.Rewrite(file, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -73,110 +68,4 @@ func main() {
 	f.Truncate(0)
 	f.Write(newSrc)
 	fmt.Printf("add trace for %s ok\n", file)
-}
-
-func hasFuncDecl(f *ast.File) bool {
-	if len(f.Decls) == 0 {
-		return false
-	}
-
-	for _, decl := range f.Decls {
-		_, ok := decl.(*ast.FuncDecl)
-		if ok {
-			return true
-		}
-	}
-
-	return false
-}
-
-func rewrite(filename string, oldSource []byte) ([]byte, error) {
-	fset := token.NewFileSet()
-	oldAST, err := parser.ParseFile(fset, filename, nil, 0)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing %s: %w", filename, err)
-	}
-	//fmt.Printf("%#v\n", *oldAST)
-
-	if !hasFuncDecl(oldAST) {
-		return nil, nil
-	}
-
-	// add import declaration
-	astutil.AddImport(fset, oldAST, "github.com/bigwhite/functrace")
-	//fmt.Printf("added=%#v\n", added)
-
-	// inject code into each function declaration
-	addDeferTraceIntoFuncDecls(oldAST)
-
-	buf := &bytes.Buffer{}
-	err = format.Node(buf, fset, oldAST)
-	if err != nil {
-		return nil, fmt.Errorf("error formatting new code: %w", err)
-	}
-	return buf.Bytes(), nil
-}
-
-func addDeferTraceIntoFuncDecls(f *ast.File) {
-	for _, decl := range f.Decls {
-		fd, ok := decl.(*ast.FuncDecl)
-		if ok {
-			// inject code to fd
-			addDeferStmt(fd)
-		}
-	}
-}
-
-func addDeferStmt(fd *ast.FuncDecl) (added bool) {
-	stmts := fd.Body.List
-
-	// check whether "defer functrace.Trace()()" has already exists
-	for _, stmt := range stmts {
-		ds, ok := stmt.(*ast.DeferStmt)
-		if !ok {
-			continue
-		}
-		// it is a defer stmt
-		ce, ok := ds.Call.Fun.(*ast.CallExpr)
-		if !ok {
-			continue
-		}
-
-		se, ok := ce.Fun.(*ast.SelectorExpr)
-		if !ok {
-			continue
-		}
-
-		x, ok := se.X.(*ast.Ident)
-		if !ok {
-			continue
-		}
-		if (x.Name == "functrace") && (se.Sel.Name == "Trace") {
-			// already exist , return
-			return false
-		}
-	}
-
-	// not found "defer functrace.Trace()()"
-	// add one
-	ds := &ast.DeferStmt{
-		Call: &ast.CallExpr{
-			Fun: &ast.CallExpr{
-				Fun: &ast.SelectorExpr{
-					X: &ast.Ident{
-						Name: "functrace",
-					},
-					Sel: &ast.Ident{
-						Name: "Trace",
-					},
-				},
-			},
-		},
-	}
-
-	newList := make([]ast.Stmt, len(stmts)+1)
-	copy(newList[1:], stmts)
-	newList[0] = ds
-	fd.Body.List = newList
-	return true
 }
---
+++ pkg/generator/rewrite.go
@@ -0,0 +1,118 @@
+package generator
+
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+
+	"golang.org/x/tools/go/ast/astutil"
+)
+
+func hasFuncDecl(f *ast.File) bool {
+	if len(f.Decls) == 0 {
+		return false
+	}
+
+	for _, decl := range f.Decls {
+		_, ok := decl.(*ast.FuncDecl)
+		if ok {
+			return true
+		}
+	}
+
+	return false
+}
+
+func Rewrite(filename string, oldSource []byte) ([]byte, error) {
+	fset := token.NewFileSet()
+	oldAST, err := parser.ParseFile(fset, filename, nil, 0)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing %s: %w", filename, err)
+	}
+	//fmt.Printf("%#v\n", *oldAST)
+
+	if !hasFuncDecl(oldAST) {
+		return nil, nil
+	}
+
+	// add import declaration
+	astutil.AddImport(fset, oldAST, "github.com/bigwhite/functrace")
+	//fmt.Printf("added=%#v\n", added)
+
+	// inject code into each function declaration
+	addDeferTraceIntoFuncDecls(oldAST)
+
+	buf := &bytes.Buffer{}
+	err = format.Node(buf, fset, oldAST)
+	if err != nil {
+		return nil, fmt.Errorf("error formatting new code: %w", err)
+	}
+	return buf.Bytes(), nil
+}
+
+func addDeferTraceIntoFuncDecls(f *ast.File) {
+	for _, decl := range f.Decls {
+		fd, ok := decl.(*ast.FuncDecl)
+		if ok {
+			// inject code to fd
+			addDeferStmt(fd)
+		}
+	}
+}
+
+func addDeferStmt(fd *ast.FuncDecl) (added bool) {
+	stmts := fd.Body.List
+
+	// check whether "defer functrace.Trace()()" has already exists
+	for _, stmt := range stmts {
+		ds, ok := stmt.(*ast.DeferStmt)
+		if !ok {
+			continue
+		}
+		// it is a defer stmt
+		ce, ok := ds.Call.Fun.(*ast.CallExpr)
+		if !ok {
+			continue
+		}
+
+		se, ok := ce.Fun.(*ast.SelectorExpr)
+		if !ok {
+			continue
+		}
+
+		x, ok := se.X.(*ast.Ident)
+		if !ok {
+			continue
+		}
+		if (x.Name == "functrace") && (se.Sel.Name == "Trace") {
+			// already exist , return
+			return false
+		}
+	}
+
+	// not found "defer functrace.Trace()()"
+	// add one
+	ds := &ast.DeferStmt{
+		Call: &ast.CallExpr{
+			Fun: &ast.CallExpr{
+				Fun: &ast.SelectorExpr{
+					X: &ast.Ident{
+						Name: "functrace",
+					},
+					Sel: &ast.Ident{
+						Name: "Trace",
+					},
+				},
+			},
+		},
+	}
+
+	newList := make([]ast.Stmt, len(stmts)+1)
+	copy(newList[1:], stmts)
+	newList[0] = ds
+	fd.Body.List = newList
+	return true
+}
